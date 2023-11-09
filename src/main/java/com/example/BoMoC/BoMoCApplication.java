23224b25d11c09dff413e1d9a0dbfec6705d2d92
phamminhdiep <hyusieunhan@gmail.com>
2023-11-09 22:54:02 +0700

    first commit

---
+++ src/main/java/com/example/BoMoC/BoMoCApplication.java
@@ -0,0 +1,13 @@
+package com.example.BoMoC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoMoCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoMoCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BoMoC/BoMoCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BoMoC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoMoCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
